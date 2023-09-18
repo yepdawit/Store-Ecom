0c073a0a8cde35288c345139a91ab5a33d69e07b
Dawit Gashaw <yepdawit@gmail.com>
2023-09-18 14:58:39 -0400

    created a logout function

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const NavBar = ({ categories }) => {
+const NavBar = ({ categories, loggedInUser, handleLogout }) => {
   return (
     <nav>
       <div>
         <Link to="/">Home</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
         <Link to="/cart">Cart</Link>
       </div>
+
+      <div>
+        {loggedInUser ? (
+          <>
+            <span>Hello, {loggedInUser}</span>
+            <button onClick={handleLogout}>Logout</button>
+          </>
+        ) : (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
+      </div>
+
       <div>
         <span>Categories:</span>
         {categories.map((category) => (
